e2940419338e8ed3834bdf51993c1e40887c3440
matteo-angelini <matteo.angelini@outlook.com>
2022-03-14 13:41:20 +0100

    Add PaymentType popup window

--- frontend/src/main/java/com/sailingclub/frontend/Helpers.java
+++ frontend/src/main/java/com/sailingclub/frontend/Helpers.java
@@ -2,7 +2,10 @@ package com.sailingclub.frontend;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import messageManagement.Message;
@@ -10,6 +13,7 @@ import messageManagement.Reply;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Helpers {
     public static Scene staticScene;
@@ -32,6 +36,41 @@ public class Helpers {
         Scene stageScene = new Scene(comp, 300, 100);
         newStage.setScene(stageScene);
         newStage.showAndWait();
+    }
+
+    /**
+     * Popup window to let the user decide whether to pay with credit card or bank transfer
+     * @param paymentType Reference used to decide which choice made the user
+     */
+    public static void showPaymentType(AtomicReference<String> paymentType) {
+        Stage newStage = new Stage();
+        VBox comp = new VBox();
+        Label label = new Label();
+        label.setText("Choose Payment Type");
+
+        RadioButton creditCard = new RadioButton("Credit Card");
+        RadioButton bankTransfer = new RadioButton("Bank Transfer");
+        ToggleGroup radioGroup = new ToggleGroup();
+        creditCard.setToggleGroup(radioGroup);
+        bankTransfer.setToggleGroup(radioGroup);
+
+        Button button = new Button("SELECT");
+
+        comp.getChildren().add(label);
+        comp.getChildren().add(creditCard);
+        comp.getChildren().add(bankTransfer);
+        comp.getChildren().add(button);
+        comp.setAlignment(Pos.CENTER);
+
+        Scene stageScene = new Scene(comp, 400, 300);
+        newStage.setScene(stageScene);
+        newStage.showAndWait();
+
+        button.setOnAction(value ->  {
+            RadioButton selectedRadioButton = (RadioButton) radioGroup.getSelectedToggle();
+            paymentType.set(selectedRadioButton.getText());
+            newStage.close();
+        });
     }
 
     /**
